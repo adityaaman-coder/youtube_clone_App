b221b75bfda5627c59f0efb998b665726d9c11f1
adityaaman-coder <adityaaman400@gmail.com>
2025-07-28 09:39:45 +0530

    index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-app.get("/movie",async(req , res)=>{
+// app.get("/movie",async(req , res)=>{
 
     
-    var name = req.query.value;
-    const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDgbfDh0ugI2tFCTlEaHre9wigPFc273OA&maxResults=5&type=video&q="+name;
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        return res.json(data);
-    }catch(error){
-        console.log(error);
-    }
-})
+//     var name = req.query.value;
+//     const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDgbfDh0ugI2tFCTlEaHre9wigPFc273OA&maxResults=5&type=video&q="+name;
+//     try{
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         return res.json(data);
+//     }catch(error){
+//         console.log(error);
+//     }
+// })
        
 
 app.listen(5500, () => {
